bebf4e92d856c69a175bde915e192a942706f628
Bryzghalov Maksym <m.bryzghalov@dragonslake.com>
2023-06-04 15:13:39 +0200

    Reworked plane flight mechanic, added turbulence, camera control and expanded the map for flight tests

--- Source/ProjectFly/Pawns/BaseFlyPlane.cpp
+++ Source/ProjectFly/Pawns/BaseFlyPlane.cpp
@@ -7,140 +7,147 @@
 
 DEFINE_LOG_CATEGORY(LogBaseFlyPlane);
 
-// Sets default values
 ABaseFlyPlane::ABaseFlyPlane()
 {
-	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bCanEverTick = true;
 
-	// Create a static mesh
-	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
-	StaticMesh->SetSimulatePhysics(true);
-	SetRootComponent(StaticMesh);
+    StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+    StaticMesh->SetSimulatePhysics(true);
+    StaticMesh->SetMassOverrideInKg(NAME_None, 0.1f);   // Set the plane's mass (lower value for lighter feel)
+    StaticMesh->SetLinearDamping(5.0f);                        // Set the plane's linear damping (lower value for less drag)
+    StaticMesh->SetAngularDamping(5.0f);                       // Set the plane's angular damping (lower value for less rotational drag)
 
-	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->bInheritRoll = false;
-	CameraBoom->TargetArmLength = 400.0f;       // The camera follows at this distance behind the character
-	CameraBoom->bUsePawnControlRotation = false; // Rotate the arm based on the controller
+    SetRootComponent(StaticMesh);
 
-	// Create a follow camera
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);  // Attach the camera to the end of the boom and let the boom
-	// adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false;                                                // Camera does not rotate relative to arm
+    FocusSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("FocusPoint"));
+    FocusSceneComponent->SetupAttachment(StaticMesh);
+
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+    CameraBoom->SetupAttachment(FocusSceneComponent);
+    CameraBoom->TargetArmLength = 400.0f;
+    CameraBoom->bUsePawnControlRotation = false;
+
+    FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+    FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+    FollowCamera->bUsePawnControlRotation = false;
 }
 
-// Called to bind functionality to input
 void ABaseFlyPlane::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis("PitchControl", this, &ABaseFlyPlane::PitchControl);
-	PlayerInputComponent->BindAxis("YawControl", this, &ABaseFlyPlane::YawControl);
-	PlayerInputComponent->BindAxis("RollControl", this, &ABaseFlyPlane::RollControl);
+    PlayerInputComponent->BindAxis("PitchControl", this, &ABaseFlyPlane::PitchControl);
+    PlayerInputComponent->BindAxis("YawControl", this, &ABaseFlyPlane::YawControl);
+    PlayerInputComponent->BindAxis("RollControl", this, &ABaseFlyPlane::RollControl);
 }
 
 void ABaseFlyPlane::AddSpeed(float Speed)
 {
-	ForwardSpeed = FMath::Clamp(ForwardSpeed + Speed, MinimumPlaneSpeed, MaximumPlaneSpeed);
+    ForwardSpeed = FMath::Clamp(ForwardSpeed + Speed, MinimumPlaneSpeed, MaximumPlaneSpeed);
 }
 
 float ABaseFlyPlane::GetSpeed() const
 {
-	return ForwardSpeed;
+    return ForwardSpeed;
 }
 
-TObjectPtr<UStaticMeshComponent> ABaseFlyPlane::GetStaticMesh() const
+UStaticMeshComponent* ABaseFlyPlane::GetStaticMesh() const
 {
-	return StaticMesh;
+    return StaticMesh;
 }
 
 void ABaseFlyPlane::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
-	ForwardSpeed = MaximumPlaneSpeed;
+    AddSpeed(StartPlaneSpeed);
 }
 
 void ABaseFlyPlane::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 
-	// Check plane boost
-	if (StaticMesh->GetForwardVector().Z < 0.0)
-	{
-		if (bDiveBoost == false && !DiveCheckTimerHandle.IsValid())
-		{
-			// Start speed timer
-			GetWorld()->GetTimerManager().SetTimer(DiveCheckTimerHandle, this, &ABaseFlyPlane::DiveCheck,
-				2.0f, false);
-		}
-	}
-	else
-	{
-		// Stop dive speed timer
-		GetWorld()->GetTimerManager().ClearTimer(DiveCheckTimerHandle);
+    CalculateSpeed(DeltaTime);
+    CalculateRotation(DeltaTime);
+    UpdateCamera();
 
-		// Activate dive boost
-		if (bDiveBoost)
-		{
-			bDiveBoost = false;
-			bRiseDecreaseDeactivation = true;
-			GetWorld()->GetTimerManager().SetTimer(BoostDeactivationTimerHandle, this, &ABaseFlyPlane::BoostDeactivation,
-				5.0f, false);
-		}
-	}
-	// Calculation of the speed change depending on the inclination of the plane
-	if (StaticMesh->GetForwardVector().Z <= 0)
-	{
-		AddSpeed(-StaticMesh->GetForwardVector().Z * DiveSpeedIncreaseScalar);
-	}
-	else
-	{
-		AddSpeed(-StaticMesh->GetForwardVector().Z * RiseSpeedDecreaseScalar * (int)(!bRiseDecreaseDeactivation));
-	}
-	
-	UE_LOG(LogBaseFlyPlane, Log, TEXT("%f"), ForwardSpeed);
-
-	// Add calculated force
-	StaticMesh->AddForce(StaticMesh->GetForwardVector() * ForwardSpeed);
-
-	// Make return to initial state after roll
-	GetStaticMesh()->SetRelativeRotation(FMath::Lerp(GetStaticMesh()->GetRelativeRotation(), CameraBoom->GetForwardVector().ToOrientationRotator(), DeltaTime));
+    UE_LOG(LogBaseFlyPlane, Log, TEXT("%f"), ForwardSpeed);
 }
 
 void ABaseFlyPlane::PitchControl(float Value)
 {
-	const float TargetPitchSpeed = -Value * AirControl;
-	FVector PitchVector = TargetPitchSpeed * StaticMesh->GetRightVector();
-	StaticMesh->AddTorqueInDegrees(PitchVector);
+    const float TargetPitchSpeed = -Value * AirControl;
+    FVector PitchVector = TargetPitchSpeed * StaticMesh->GetRightVector();
+    StaticMesh->AddTorqueInRadians(PitchVector, NAME_None, true);
 }
 
 void ABaseFlyPlane::YawControl(float Value)
 {
-	const float TargetYawSpeed = Value * AirControl;
-	FVector YawVector = TargetYawSpeed * StaticMesh->GetUpVector();
-	StaticMesh->AddTorqueInDegrees(YawVector);
+    const float TargetYawSpeed = Value * AirControl;
+    FVector YawVector = TargetYawSpeed * StaticMesh->GetUpVector();
+    StaticMesh->AddTorqueInRadians(YawVector, NAME_None, true);
 }
 
 void ABaseFlyPlane::RollControl(float Value)
 {
-	const float TargetRollSpeed = Value * AirControl;
-	FVector RollVector = TargetRollSpeed * StaticMesh->GetForwardVector();
-	StaticMesh->AddTorqueInDegrees(RollVector);
+    const float TargetRollSpeed = Value * AirControl;
+    FVector RollVector = TargetRollSpeed * StaticMesh->GetForwardVector();
+    StaticMesh->AddTorqueInRadians(RollVector, NAME_None, true);
 }
 
-void ABaseFlyPlane::DiveCheck()
+void ABaseFlyPlane::CalculateSpeed(float DeltaTime)
 {
-	if (StaticMesh->GetForwardVector().Z < 0)
-	{
-		bDiveBoost = true;
-	}
+    // Calculation of the speed change depending on the inclination of the plane
+    if (StaticMesh->GetForwardVector().Z <= 0)
+    {
+        AddSpeed(-StaticMesh->GetForwardVector().Z * DiveSpeedIncreaseScalar);
+    }
+    else
+    {
+        AddSpeed(-StaticMesh->GetForwardVector().Z * RiseSpeedDecreaseScalar);
+    }
+    StaticMesh->AddForce(StaticMesh->GetForwardVector() * ForwardSpeed);
 }
 
-void ABaseFlyPlane::BoostDeactivation()
+void ABaseFlyPlane::CalculateRotation(float DeltaTime)
 {
-	bRiseDecreaseDeactivation = false;
+    const float TurbulenceValue = TurbulenceFactor / 10000.0f;
+
+    // Calculate turbulence
+    const FVector CurrentVelocity = StaticMesh->GetComponentVelocity();
+    const FVector Turbulence = FVector(
+        FMath::FRandRange(-1.0f, 1.0f),
+        FMath::FRandRange(-1.0f, 1.0f),
+        FMath::FRandRange(-1.0f, 1.0f)
+    ) * TurbulenceValue;
+
+    // Apply forces
+    StaticMesh->AddForce(Turbulence);
+
+    // Apply rotational forces
+    const FVector PitchVector = StaticMesh->GetRightVector() * TurbulenceValue * FMath::FRandRange(-1.0f, 1.0f) * ForwardSpeed;
+    const FVector YawVector = StaticMesh->GetUpVector() * TurbulenceValue * FMath::FRandRange(-1.0f, 1.0f) * ForwardSpeed;
+    const FVector RollVector = StaticMesh->GetForwardVector() * TurbulenceValue * FMath::FRandRange(-1.0f, 1.0f) * ForwardSpeed;
+    StaticMesh->AddTorqueInRadians(PitchVector, NAME_None, true);
+    StaticMesh->AddTorqueInRadians(YawVector, NAME_None, true);
+    StaticMesh->AddTorqueInRadians(RollVector, NAME_None, true);
+
+    // Beginning to reduce the pitch angle of the airplane after reaching a speed below a certain value
+    if (ForwardSpeed <= PlaneSpeedThresholdForPitchDecline)
+    {
+        // Make static mesh slowly face downwards
+        GetStaticMesh()->SetRelativeRotation(FMath::Lerp(GetStaticMesh()->GetRelativeRotation(), FVector(0.0f, 0.0f, -1.0f).ToOrientationRotator(), DeltaTime / 10));
+    }
+
+    // Make return to initial state after roll
+    GetStaticMesh()->SetRelativeRotation(FMath::Lerp(GetStaticMesh()->GetRelativeRotation(), CameraBoom->GetForwardVector().ToOrientationRotator(), DeltaTime * AutoRotationPlaneSpeedScalar));
+
+}
+
+void ABaseFlyPlane::UpdateCamera()
+{
+    // Update camera boom length based on forward speed
+    const float SpeedPercentage = FMath::GetMappedRangeValueClamped(FVector2D(MinimumPlaneSpeed, MaximumPlaneSpeed), FVector2D(0.0f, 1.0f), ForwardSpeed);
+    const float CameraBoomLength = FMath::Lerp(MinimumCameraBoomLength, MaximumCameraBoomLength, SpeedPercentage);
+    CameraBoom->TargetArmLength = CameraBoomLength;
 }
