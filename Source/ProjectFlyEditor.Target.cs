a59a9da56be7e404a4a95c91926cc38ec1346267
Bryzghalov Maksym <m.bryzghalov@dragonslake.com>
2025-06-09 22:12:10 +0300

    Updated Unreal Engine version to 5.6

--- Source/ProjectFlyEditor.Target.cs
+++ Source/ProjectFlyEditor.Target.cs
@@ -8,7 +8,8 @@ public class ProjectFlyEditorTarget : TargetRules
 	public ProjectFlyEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "ProjectFly" } );
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+        ExtraModuleNames.AddRange( new string[] { "ProjectFly" } );
 	}
 }
